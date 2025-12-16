6bf6f5c5e07df44cba55684819f238623df4206a
elfrieddasilva <elfrieddasilva@gmail.com>
2025-12-16 08:05:00 +0100

    Day 2: successfully read binary file

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+
 
 int main() {
-    std::cout << "cpp-mini-jvm started\n";
+    std::ifstream file("test.bin", std::ios::binary);
+
+    if (!file) {
+        std::cerr << "Failed to open file\n";
+        return 1;
+    }
+
+    std::vector<uint8_t> bytes(
+        (std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>()
+    );
+
+    std::cout << "Read " << bytes.size() << " bytes\n";
+
     return 0;
+
 }
